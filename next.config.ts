00f847c08cdc47f8463cc6e7fa244deb04e43bd9
JorgeGcode <94862601+JorgeGcode@users.noreply.github.com>
2026-01-20 14:17:33 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
-  /* config options here */
-  /** @type {import('next').NextConfig} */
-  output: 'standalone', // <--- ESTA ES LA LÍNEA MÁGICA
-};
 
-module.exports = nextConfig;
+const nextConfig: NextConfig = {
+  output: 'standalone',
 };
 
 export default nextConfig;
